32af13635f4eefba76759c5265a00e6b1aa6f4f5
j-Pej <pejiguillermojj@gmail.com>
2023-08-17 10:37:08 +0800

    Added key

--- assets/js/dynamic.js
+++
@@ -1,4 +0,0 @@
-// Accessing elements in HTML and updating their content using JSON data
-document.getElementById("category").textContent = jsonData.title;
-document.getElementById("score").textContent = jsonData.score;
-document.getElementById("icon").textContent = jsonData.icon;
